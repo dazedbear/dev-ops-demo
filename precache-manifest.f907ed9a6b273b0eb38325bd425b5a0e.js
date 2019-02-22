649631ede4baeae98805200ee5bb81f2bf347e8e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-22 10:42:46 +0000

    Deploy dazedbear/dev-ops-demo to github.com/dazedbear/dev-ops-demo.git:gh-pages

---
+++ precache-manifest.f907ed9a6b273b0eb38325bd425b5a0e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "26add5f169e551697cc1",
+    "url": "/static/js/main.26add5f1.chunk.js"
+  },
+  {
+    "revision": "c88c70e5f4ff8bea6fac",
+    "url": "/static/js/2.c88c70e5.chunk.js"
+  },
+  {
+    "revision": "26add5f169e551697cc1",
+    "url": "/static/css/main.c3781e40.chunk.css"
+  },
+  {
+    "revision": "a83f7cfe20b03164d689af1a41de6a27",
+    "url": "/index.html"
+  }
+];
--- src/init.js
+++
@@ -1,6 +0,0 @@
-const init = (num) => {
-  if (typeof num !== 'number') return false;
-  return num * 100;
-}
-
-export default init;
